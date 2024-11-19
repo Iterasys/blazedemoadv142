b5df7e0bd0ea5d7fd0efffc828f20e5fd69397c7
dh23iterasys <dh23@iterasys.com.br>
2024-11-18 21:16:15 -0300

    Final Page Objects

--- src/test/java/stepsPO/ComprarPassagemPO.java
+++ src/test/java/stepsPO/ComprarPassagemPO.java
@@ -16,16 +16,16 @@ import pages.Base;
 
 public class ComprarPassagemPO {
     // Atributos - Ponto Final / Estação Final / Terminal
-    final WebDriver driver;           // Selenium só pode usar e devolver 
-                                      // 1 - declarar
-    private HomePage homePage;        // Só eu posso usar a Home
-    private ReservePage reservePage;  // Só eu posso usar a Reserve
+    final WebDriver driver; // Selenium só pode usar e devolver
+                            // 1 - declarar
+    private HomePage homePage; // Só eu posso usar a Home
+    private ReservePage reservePage; // Só eu posso usar a Reserve
 
     public String origem;
     public String destino;
 
     // Constructor
-    public ComprarPassagemPO(Base base){
+    public ComprarPassagemPO(Base base) {
         this.driver = base.driver;
     }
 
@@ -34,10 +34,10 @@ public class ComprarPassagemPO {
     @Dado("que acesso o site {string} PO")
     public void que_acesso_o_site_po(String url) {
         homePage = new HomePage(driver); // 2 - instanciar
-        homePage.acessarHomePage(url);   // 3 - usar
+        homePage.acessarHomePage(url); // 3 - usar
         // validar se abriu a página correta através do nome da guia
         Assert.assertEquals("BlazeDemo", homePage.lerNomeDaGuia());
-        
+
     }
 
     @Quando("seleciono a {string} e {string} PO")
@@ -46,8 +46,18 @@ public class ComprarPassagemPO {
         this.destino = destino;
         homePage.selecionarOrigemDestino(origem, destino);
 
-        // ToDo: Na preparação de aula há um ajuste de sincronismo
-        // é para conseguirmos visualizar o robô
+        // Ativar a sincronização para o robô executar devagar
+        // E podermos visualizar o funcionamento
+        // Importante: É só como curiosidade ou em caso de problemas
+        // O indicado é deixar o robô executar o mais rápido possível
+
+        synchronized (driver) {
+            try {
+                driver.wait(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @E("clico no botao Find Flights PO")
@@ -55,16 +65,36 @@ public class ComprarPassagemPO {
         homePage.clicarBotaoFindFlights();
         // chama a página sequinte --> Reserve
         reservePage = new ReservePage(driver);
+
     }
 
     @Entao("visualiza a lista de voos PO")
     public void visualiza_a_lista_de_voos_po() {
         Assert.assertEquals("BlazeDemo - reserve", reservePage.lerNomeDaGuia());
         Assert.assertEquals("Flights from " + this.origem + " to " + this.destino + ":",
-          reservePage.lerCabecalhoVoos());
+                reservePage.lerCabecalhoVoos());
 
+        synchronized (driver) {
+            try {
+                driver.wait(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
+    @Quando("clico no {int} PO")
+    public void clico_no_po(Integer ordem_do_voo) {
+        reservePage.clicarNoVoo(ordem_do_voo);
+
+        synchronized (driver) {
+            try {
+                driver.wait(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
